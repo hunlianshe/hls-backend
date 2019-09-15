f675f95c22a606750fcda8fb121566ae5e06ac8e
刘祖宽 <18818216454@163.com>
2019-09-15 16:55:14 +0800

    new detail

--- src/app/fortune/fortune.service.ts
+++ src/app/fortune/fortune.service.ts
@@ -1,17 +1,55 @@
 import { Injectable } from '@nestjs/common'
 import { IFortune, Fortune } from '../../models/fortune'
 import { ClientService } from '../../lib/client.service'
+import { UserDetail } from '../../models/user_detail'
+import { RulesService } from '../../lib/rules.service'
 
 @Injectable()
 export class FortuneService {
   async findByName(name: string): Promise<IFortune> {
     return await Fortune.findOne({ name })
   }
+
   async getHoroscope(params: string): Promise<IFortune> {
     return await ClientService.getHoroscope(params)
   }
+
   async constellationMmatching(params: string): Promise<IFortune> {
     return await ClientService.constellationMmatching(params)
+  }
+
+  async constellationMmatchingComplex(openid: string): Promise<any> {
+    let user = await UserDetail.findOne({ openid })
+    let model = {}
+    let opposite
+    if (user.gender === 1) {
+      opposite = UserDetail.findOne({
+        gender: 2,
+        'objectInfo.salary': { $exists: true },
+      })
+      model = {
+        salary: RulesService.generateSalaryScore(user.salary, opposite.salary),
+        height: RulesService.generateSalaryScore(user.height, opposite.height),
+        age: RulesService.generateSalaryScore(user.age, opposite.age),
+        star: await ClientService.constellationMmatching(
+          `?me=${user.constellation}&he=${opposite.constellation}`,
+        ),
+      }
+    } else {
+      opposite = UserDetail.findOne({
+        gender: 1,
+        'objectInfo.salary': { $exists: true },
+      })
+      model = {
+        salary: RulesService.generateSalaryScore(opposite.salary, user.salary),
+        height: RulesService.generateSalaryScore(opposite.height, user.height),
+        age: RulesService.generateSalaryScore(opposite.age, user.age),
+        star: await ClientService.constellationMmatching(
+          `?me=${opposite.constellation}&he=${user.constellation}`,
+        ),
+      }
+    }
+    return model
   }
 
   async updateDoc(): Promise<void> {
--- src/lib/client.service.ts
+++ src/lib/client.service.ts
@@ -61,6 +61,7 @@ export class ClientService {
   }
 
   static async constellationMmatching(params): Promise<any> {
+    params = params.replace(/座/gi, '')
     let url = `${config.TIANAPI.HOST}/txapi/xingzuo?${params}&key=${config.TIANAPI.KEY}`
     if (map.get(url + moment().format('YYYYMMDD')))
       return map.get(url + moment().format('YYYYMMDD'))
