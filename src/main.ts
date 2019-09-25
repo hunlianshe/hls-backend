f8fc86d29aa5c7cf19a71532ef4c98a4262a2b87
刘祖宽 <18818216454@163.com>
2019-09-25 23:19:37 +0800

    username or password wrong

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { HttpExceptionFilter } from './filters/http-exception.filter'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept')
+    next()
+  })
   app.useGlobalInterceptors(new TransformInterceptor())
   app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(8009)
