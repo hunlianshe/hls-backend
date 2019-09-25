fe481ef5a765dd6fda75f9bcdaf39f104f89297f
刘祖宽 <18818216454@163.com>
2019-09-25 23:18:18 +0800

    username or password wrong

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { TransformInterceptor } from './interceptor/transform.interceptor'
 import { HttpExceptionFilter } from './filters/http-exception.filter'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule)
+  app.enableCors()
   app.useGlobalInterceptors(new TransformInterceptor())
   app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(8009)
