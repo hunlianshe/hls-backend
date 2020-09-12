cd58b84a0951a1667589c2c7737bb88eb49429a2
Jiqing Liu <jiqing.j.liu@pwc.com>
2020-09-13 00:23:29 +0800

    [master] add the order pay module

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,7 @@ import { NestFactory } from '@nestjs/core'
 import { GroupService } from './app/group/group.service'
 import { GroupController } from './app/group/group.controller'
 import { GroupModule } from './app/group/group.module'
-import { GroupModule } from './app/group/group.module'
+import { OrderPayModule } from './app/order-pay/order-pay.module'
 
 @Module({
   imports: [
@@ -21,6 +21,7 @@ import { GroupModule } from './app/group/group.module'
     PsychologicalTestModule,
     ConstellationStoryModule,
     GroupModule,
+    OrderPayModule,
   ],
   controllers: [AppController, GroupController],
   providers: [AppService, GroupService],
@@ -30,5 +31,6 @@ export class AppModule implements NestModule {
     consumer.apply(AuthMiddleware).forRoutes('users')
     consumer.apply(AuthMiddleware).forRoutes('fortune')
     consumer.apply(AuthMiddleware).forRoutes('psychological-test')
+    consumer.apply(AuthMiddleware).forRoutes('orderPay')
   }
 }
